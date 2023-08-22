286a200a29e7a6323b9ed9f3cfaf0b8fb407fac8
Taylorm1011 <111470063+Taylorm1011@users.noreply.github.com>
2023-08-22 12:11:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const fs = require("fs");
+
+//server configiguration
+const hostname = "127.0.0.1";
+const port = 3200;
+
+
+// reading the files
+const homePage = fs.readFileSync("home.html");
+const aboutPage = fs.readFileSync("about.html");
+const menuPage = fs.readFileSync("menu.html");
+const wherePage = fs.readFileSync("where.html");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(homePage);
+        } else if (req.url === "/home.html") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(aboutPage);
+        } else if (req.url === "/about.html") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(menuPage);
+        } else if (req.url === "/menu.html") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(wherePage);
+        }
+        else if(req.url.match("\.jpg")) {
+            try {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "cafeHeader/jpg");
+                imgLoc = req.url.replace("/","./");
+                console.log(imgLoc);
+                image = fs.readFileSync(imgLoc);
+                res.end(image);
+            } catch {
+                res.statusCode = 404;
+                res.write("404");
+                console.log(req.url);
+            }
+            
+        } else {
+            res.statusCode = 404;
+            res.write("404");
+            console.log(req.url);
+        }
+        res.end();
+    
+});  
+// this starts the server 
+server.listen(port, hostname, () => {
+    console.log("server is now running");
+});
+
+
+
+    
+        
+
+
